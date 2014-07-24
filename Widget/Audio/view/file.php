c64e17c9760788d22dbe6685cac73fb1e563bdb2
Mangirdas Skripka <mangirdas@impresspages.org>
2014-07-24 19:06:05 +0300

    Preloading sometimes hangs the browser.

--- Widget/Audio/view/file.php
+++ Widget/Audio/view/file.php
@@ -1,5 +1,5 @@
 <div  data-toggle="tooltip" title="<?php echo esc($fileName); ?>">
-    <audio controls>
+    <audio controls preload="none">
         <source src="<?php echo esc($fileUrl); ?>">
     </audio>
 </div>
